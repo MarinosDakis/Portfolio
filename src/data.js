b186a7c9337959950910b1549bcf6a5ea5317263
MarinosDakis <54782000+MarinosDakis@users.noreply.github.com>
2021-08-24 13:12:06 -0400

    changed some css and added more projects

--- src/data.js
+++ src/data.js
@@ -9,30 +9,36 @@ export const featuredPortfolio = [
     },
     {
       id: 2,
+      title: "Memories Web app",
+      img: "assets/project images/memories.png",
+      url: "https://silly-minsky-872be3.netlify.app/",
+    },
+    {
+      id: 3,
       title: "Philo4Thought Website",
       img: "assets/project images/P4T 2020 Logo Square.jpg",
       url: "https://www.philo4thought.org/",
     },
     {
-      id: 3,
+      id: 4,
       title: "Covid-19 Data Challenge",
       img: "assets/project images/virus.png",
       url: "https://drive.google.com/drive/folders/1PYz0QVXH8OBVOPLXHcoNF3mcu90ZfzM4?usp=sharing",
     },
     {
-      id: 4,
+      id: 5,
       title: "Best Fit Simulation",
       img: "assets/project images/best fit sim.png",
       url: "https://github.com/MarinosDakis/Operating-Systems-CS340/tree/master/Best%20Fit%20Simulation",
     },
     {
-      id: 5,
+      id: 6,
       title: "Binary Expression Tree",
       img: "assets/project images/expression tree.png",
       url: "https://github.com/MarinosDakis/Expression-Tree",
     },
     {
-      id: 6,
+      id: 7,
       title: "URL Extractor",
       img: "assets/project images/url extractor.png",
       url: "https://github.com/MarinosDakis/Software-Engineering-370",
@@ -55,9 +61,9 @@ export const featuredPortfolio = [
       },
       {
         id: 3,
-        title: "Weather API Web App",
-        img: "assets/project images/weather.jpg",
-        url: "https://marinosdakis.github.io/Weather%20Application/weather.html",
+        title: "Memories Web app",
+        img: "assets/project images/memories.png",
+        url: "https://silly-minsky-872be3.netlify.app/",
       },
       {
         id: 4,
@@ -68,7 +74,7 @@ export const featuredPortfolio = [
       {
         id: 5,
         title: "Speed Test Typer",
-        img: "assets/project images/speed test.png",
+        img: "assets/project images/speed.png",
         url: "https://marinosdakis.github.io/Typing%20speed%20tester%20(LinkedIn%20Learning%20Week%206)/index.html",
       },
       {
@@ -76,6 +82,12 @@ export const featuredPortfolio = [
         title: "File Metadata Microservice",
         img: "assets/project images/file metadata.png",
         url: "https://boilerplate-project-filemetadata--marinosdakis.repl.co/",
+      },
+      {
+        id: 7,
+        title: "Weather API Web App",
+        img: "assets/project images/weather.jpg",
+        url: "https://marinosdakis.github.io/Weather%20Application/weather.html",
       },
   ];
   
